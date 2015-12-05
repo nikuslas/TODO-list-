e7e1bff0679828d745d07ed48cf350694acf24e1
Markus Nicolas <nicolas.markus@etu.u-pec.fr>
2015-12-05 17:23:52 +0000

    TODO list works with add button and core data save all data

--- TODOlist/ViewController.swift
+++ TODOlist/ViewController.swift
@@ -7,19 +7,128 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
+    //Creat tableView for TODO list
+    @IBOutlet weak var TODOList: UITableView!
+    
+    var item = [NSManagedObject]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        title = "\"The TODO List\""
+        TODOList.registerClass(UITableViewCell.self,
+            forCellReuseIdentifier: "Cell")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func tableView(tableView: UITableView,
+        numberOfRowsInSection section: Int) -> Int {
+            return item.count
+    }
+    
+    func tableView(tableView: UITableView,
+        cellForRowAtIndexPath
+        indexPath: NSIndexPath) -> UITableViewCell {
+            
+            let cell =
+            tableView.dequeueReusableCellWithIdentifier("Cell")
+            
+            let todoitem = item[indexPath.row]
+            
+            cell!.textLabel!.text =
+                todoitem.valueForKey("itemtodo") as? String
+            
+            return cell!
+    }
+    
+    func saveName(name: String) {
+        
+        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        
+        let managedContext = appDelegate.managedObjectContext
+        
+        let entity =  NSEntityDescription.entityForName("Item", inManagedObjectContext:managedContext)
+        
+        let iitem = NSManagedObject(entity: entity!, insertIntoManagedObjectContext: managedContext)
+        
+        iitem.setValue(name, forKey: "itemtodo")
+        
+        do {
+            try managedContext.save()
+            item.append(iitem)
+        } catch let error as NSError  {
+            print("Could not save \(error), \(error.userInfo)")
+        }
+    }
 
+    
+    //Creat button action to save items in TODO list
+    @IBAction func AddItemTODO(sender: AnyObject) {
+        
+        let alert = UIAlertController(title: "New Name",
+            message: "Add a new name",
+            preferredStyle: .Alert)
+        
+        let saveAction = UIAlertAction(title: "Save",
+            style: .Default,
+            handler: { (action:UIAlertAction) -> Void in
+                
+                let textField = alert.textFields!.first
+                self.saveName(textField!.text!)
+                self.TODOList.reloadData()
+        })
+        
+        let cancelAction = UIAlertAction(title: "Cancel",
+            style: .Default) { (action: UIAlertAction) -> Void in
+        }
+        
+        alert.addTextFieldWithConfigurationHandler {
+            (textField: UITextField) -> Void in
+        }
+        
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        
+        presentViewController(alert,
+            animated: true,
+            completion: nil)
+        
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let appDelegate =
+        UIApplication.sharedApplication().delegate as! AppDelegate
+        
+        let managedContext = appDelegate.managedObjectContext
+    
+        let fetchRequest = NSFetchRequest(entityName: "Item")
+        
+        do {
+            let results =
+            try managedContext.executeFetchRequest(fetchRequest)
+            item = results as! [NSManagedObject]
+        } catch let error as NSError {
+            print("Could not fetch \(error), \(error.userInfo)")
+        }
+    }
 
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == UITableViewCellEditingStyle.Delete {
+            item.removeAtIndex(indexPath.row)
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: UITableViewRowAnimation.Automatic)
+        }
+    }
+    
 }
 
